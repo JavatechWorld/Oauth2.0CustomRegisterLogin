f52e3ac33f4360c42d87a2266aef07d4e7b87265
SCantore95 <58028990+SCantore95@users.noreply.github.com>
2024-05-17 19:06:30 +0200

    Update Role.java

--- src/main/java/com/oauth/implementation/model/Role.java
+++ src/main/java/com/oauth/implementation/model/Role.java
@@ -1,10 +1,11 @@
 package com.oauth.implementation.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 
 @Entity
 @Table(name="role")
