71db565336c368667527658fb14f7bfda4b7bcad
SCantore95 <58028990+SCantore95@users.noreply.github.com>
2024-05-17 19:04:27 +0200

    Update CustomSuccessHandler.java

--- src/main/java/com/oauth/implementation/config/CustomSuccessHandler.java
+++ src/main/java/com/oauth/implementation/config/CustomSuccessHandler.java
@@ -2,9 +2,9 @@ package com.oauth.implementation.config;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
